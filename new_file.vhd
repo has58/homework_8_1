76ec7ca8dc87a6d61fdf540aa2d1c744722567a0
has58 <has58@njit.edu>
2019-04-08 19:29:44 -0400

    final pachake

--- new_file.vhd
+++ new_file.vhd
@@ -4,11 +4,22 @@ use ieee.std_logic_1164.ALL;
 use work.Network_Coding.ALL;
 entity new_en is
 	port (cin_1, cin_2 : in std_logic;
-		cout : out std_logic);
+		cout1,cout2,cout3 : out integer);
 end new_en;
 
 
 Architecture new_arch of new_en is
+	signal a : std_logic;
+	constant b : integer :=3;
 	begin
-		cout <= encoding(cin_1,cin_2);
+		a <= encoding(cin_1,cin_2);
+	p1:	process(a)
+			constant b : integer :=5;
+			begin
+				if (a='1') then
+					cout1 <= b;
+					cout2 <= new_arch.b;
+					cout3 <= p1.b;
+				end if;
+			end process p1;
 	end new_arch;
